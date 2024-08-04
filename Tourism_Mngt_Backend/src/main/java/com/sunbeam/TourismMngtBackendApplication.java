def0634b4d487b4ca1b44964e6cf6ae7c1fd69c0
SWakte <shubhamwakte@gmail.com>
2024-08-04 16:32:54 +0530

    commited

--- Tourism_Mngt_Backend/src/main/java/com/sunbeam/TourismMngtBackendApplication.java
+++ Tourism_Mngt_Backend/src/main/java/com/sunbeam/TourismMngtBackendApplication.java
@@ -6,6 +6,8 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TourismMngtBackendApplication {
@@ -19,6 +21,19 @@ public class TourismMngtBackendApplication {
 		ModelMapper modelMapper = new ModelMapper();
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		return modelMapper;
+	}
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+	                    .allowedOrigins("http://localhost:3000")
+	                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+	                    .allowedHeaders("Content-Type", "Accept", "Accept-Language", "Accept-Encoding");
+	        }
+	    };
 	}
 
 }
