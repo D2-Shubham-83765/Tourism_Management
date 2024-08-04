b39e9d28e9048a16a004ae3f6b5f4a4d099f1faa
sunbeam-pdac <pmvyavhare@gmail.com>
2024-08-04 15:08:57 +0530

    Updated homepage

---
+++ tourism_mngt/src/services/bookingData.js
@@ -0,0 +1,11 @@
+const bookingData ={
+    packageName: "Adventure",
+    destination: "Inspiring Ladakh",
+    startDate: "20/07/2024",
+    endDate: "26/07/2024",
+    travelers: "4",
+    packagePrice: "25000",
+    totalPrice: "100000"
+}
+
+export default bookingData
